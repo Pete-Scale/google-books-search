d75df5f1647c6faa91822e360c1abb4b884b38b2
Pete-Scale <petescale@gmail.com>
2021-04-13 14:48:50 -0500

    basic front end setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+import Search from './pages/search'
+import Saved from './pages/saved'
+import Nav from './components/nav/nav'
+import Header from './components/header/header'
+
 function App() {
   return (
-    <div>App.js</div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/saved">
+          <Header title="Your Saved Books" />
+          <Saved />
+        </Route>
+        <Route path="/">
+          <Header title="Search For New Books" />
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
