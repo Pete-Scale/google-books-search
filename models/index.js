e7d25edb4235b470cf9cf5469fb5b7e38438f3a1
Pete-Scale <petescale@gmail.com>
2021-04-13 11:33:14 -0500

    required in mongoose on server

--- models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Drawing: require("./books.js"),
-  User: require("./user.js"),
-};
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Connect to mongoose database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useFindAndModify: false,
